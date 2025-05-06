ee571f2e34ebbc17fbb39406b94b6aee4d162e67
nitro <qpp@boxfi.uk.com>
2025-05-06 13:44:24 -0500

    Fix ADMIN_ID import

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 NOWPAYMENTS_API_KEY = os.getenv("NOWPAYMENTS_API_KEY")
 WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
 BASE_URL = os.getenv("BASE_URL")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+ADMIN_ID = int(os.getenv("ADMIN_TELEGRAM_ID"))
 OWNER_ID = 1217831346  # your Telegram ID
