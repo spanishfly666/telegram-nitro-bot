52d7cbfafb862bdffa74c14f885c30aa2c1f7b8b
nitro <qpp@boxfi.uk.com>
2025-05-08 04:09:47 -0500

    Added migrate_username.py for username column migration

---
+++ migrate_username.py
@@ -0,0 +1,27 @@
+from
+flask
+import
+Flask
+from
+flask_sqlalchemy
+import
+SQLAlchemy
+import
+os
+app.config[SQLALCHEMY_DATABASE_URI]
+=
+os.getenv
+DATABASE_URL
+postgresql://localhost/nitro_bot
+app.config[SQLALCHEMY_TRACK_MODIFICATIONS]
+=
+False
+try:
+db.session.execute
+ALTER TABLE users ALTER COLUMN username TYPE VARCHAR(256)
+print
+Successfully altered username column to VARCHAR(256)
+except
+Exception
+as
+e:
